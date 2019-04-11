49bb40b7a113751946545d1f527fd056a0784446
Liz Tiller <elizabeth.tiller@gmail.com>
2019-04-11 14:52:18 -0400

    memory hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  const [data, setData] = useState()
+  const [difficulty, setDifficulty] = useState(0)
+  const [gameId, setGameId] = useState(0)
+  const [board, setBoard] = useState([])
+  const [gameState, setGameState] = useState('start')
+
+  const minesweeperAPI = 'http://minesweeper-api.herokuapp.com/games?difficulty'
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.post(
+        `${minesweeperAPI}=${difficulty}`,
+      )
+      setData(result.data)
+      setGameId(result.data.id)
+      setBoard(result.data.board)
+      setGameState(result.data.state)
+    };
+    fetchData()
+  }, {})
+
+  console.log(gameState)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MineSweeper</h1>
+        <h2>{gameId}</h2>
+        <h2>{gameState}</h2>
+      </header>
+    </div>
+  );
 }
 
 export default App;
